b305985e3a2a34f511c2251a530572ec59e3ff40
dipankarpaul2k <dipankarpaul2k@gmail.com>
2024-05-25 14:12:08 +0530

    add portfolio link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,11 @@ const Footer = () => {
     <footer className="text-center mt-4 py-4">
       <p>
         Made by me,{" "}
-        <Link href={"/"} className="hover:underline hover:text-blue-700">
+        <Link
+          href={"https://dipankarpaul.vercel.app/"}
+          className="hover:underline hover:text-blue-700"
+          target="_blank"
+        >
           {" "}
           Dipankar Paul
         </Link>
