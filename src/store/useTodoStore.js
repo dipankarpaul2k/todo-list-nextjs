17ccc0f3e99f38502c65af5a75cccd0a6575bdbc
dipankarpaul2k <dipankarpaul2k@gmail.com>
2024-05-25 11:51:49 +0530

    add addTodo and removeTodo functions

--- src/store/useTodoStore.js
+++ src/store/useTodoStore.js
@@ -3,6 +3,9 @@ import { create } from "zustand";
 const useTodoStore = create((set) => ({
   todos: [],
   setTodos: (todos) => set({ todos }),
+  addTodo: (newTodo) => set((state) => ({ todos: [...state.todos, newTodo] })),
+  removeTodo: (id) =>
+    set((state) => ({ todos: state.todos.filter((todo) => todo._id !== id) })),
 }));
 
 export default useTodoStore;
